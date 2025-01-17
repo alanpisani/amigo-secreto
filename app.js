d0058eb6ccd2e89fdd883550529435e620e506de
Alan Pisani <alanpisani1993@gmail.com>
2025-01-16 23:10:20 -0300

    creada funcion retornarAmigos

--- app.js
+++ app.js
@@ -4,13 +4,25 @@ let amigos = [];
 
 function agregarAmigo(){
     const amigo = document.getElementById("amigo");
-    console.log(amigo.value);
+    //console.log(amigo.value);
 
     if(amigo.value == ""){
         alert("Por favor, ingrese un nombre");
     }else{
-        amigos.push(amigo);
+        amigos.push(amigo.value);
     }
     amigo.value = "";
+    retornarAmigos();
 }
 
+function retornarAmigos(){
+    const listaAmigos = document.getElementById("listaAmigos");
+    let lis = "";
+    console.log(amigos);
+    listaAmigos.innerHTML = "";
+
+    for(let i = 0; i < amigos.length; i++){
+        lis += `<li>${amigos[i]}</li>`
+    }
+    listaAmigos.innerHTML = lis;
+}
